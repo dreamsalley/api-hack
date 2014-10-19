400a413a98a7af9ff8478d629405e881e80207e1
csdgrevolution <CreativeStar@tisean-bells-macbook.local>
2014-10-19 13:59:27 -0400

    changes

--- js/app.js
+++ js/app.js
@@ -1,48 +1,19 @@
-document.onload = function() {
-  SC.initialize({
-    client_id: "'YOUR_CLIENT_ID"
-  });
-};
-
-function getTracks(q) {
-  SC.get("/tracks", {
-    q: q
-  }, function(tracks) {
-    console.log(tracks);
-  });
-};
-
-function getQ() {
-  //var a = document.getElementById("song-input");
-  var b = $("#song-input").val();
-  var noName = /\S\w/;
-  console.log(b);
-    if(!noName.test(b) || b == ""){
-      console.log(b);
-      return false;
-    } else {
-      console.log(b);
-      $("song-input").html("<h1>Searching for:" + b +"</h1>");
-      return b;
-    };
-    
-  };
-
-$(document).ready(function() {
-  $("#song-input").on("keyDown", function() {
-    event.preventDefault();
-    var nQ = getQ();
-    var validQuery = someQuery(nQ);
-    getTracks(validQuery);
-  });
-  
-  $("#song-input").on("keyUp", function(event) {
-    if(event.which == 13){
-      event.preventDefault();
-      var nQ = getQ();
-      var validQuery = someQuery(nQ);
-      getTracks(validQuery);
-    };
-  
-  });
-});
+//http://developer.echonest.com/api/v4/artist/search?api_key=FILDTEOIK2HBORODV&format=json&results=10&name=train&bucket=id:whosampled
+$.ajax({
+  url: 'http://developer.echonest.com/api/v4/artist/search',
+  dataType: 'json',
+  type: 'GET',
+  data: {
+    api_key: 'GASA0WIIJLEC8ZPM4',
+    format: 'jsonp',
+    results: 10,
+    name: 'train',
+    bucket: 'id:whosampled'
+  },
+  success: function(data){
+    console.log('success',data);
+  },
+  error: function(data){
+    console.log('error',data);
+  }
+})
