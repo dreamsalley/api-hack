a0ac7bbe8f9bb4fa6ce65a0f0124164b9faca488
csdgrevolution <CreativeStar@tisean-bells-macbook.local>
2014-10-21 18:16:16 -0400

    changes

--- js/app.js
+++ js/app.js
@@ -1,19 +1,19 @@
  //http://developer.echonest.com/api/v4/artist/search?api_key=FILDTEOIK2HBORODV&format=json&results=10&name=train&bucket=id:whosampled
- 
- $$(document).ready(function() {
- 	$('input').keydown(function(e) {
-      if (e.which == 13) {
-          whosampled($(this).val());
-       }
-   });
- });
- function whosampled(tag){
- 	$.ajax({
-     url: 'http://developer.echonest.com/api/v4/artist/search',
+$(function() {
+    $('input').keydown(function(e) {
+        if (e.which == 13) {
+            whosampled($(this).val());
+        }
+    });
+});
+
+function whosampled(tag){
+    $.ajax({
+      url: 'http://developer.echonest.com/api/v4/artist/search',
       dataType: 'json',
-     type: 'GET',
-           data: {
-    api_key: 'GASA0WIIJLEC8ZPM4',
+      type: 'GET',
+      data: {
+        api_key: 'GASA0WIIJLEC8ZPM4',
         results: 10,
         name: tag,
         bucket: 'id:whosampled'
