93dad6d08ea27b757610737a311cbb67e33614d4
csdgrevolution <CreativeStar@tisean-bells-macbook.local>
2014-10-25 14:41:18 -0400

    changes

--- js/app.js
+++ js/app.js
@@ -1,4 +1,4 @@
- //http://developer.echonest.com/api/v4/artist/search?api_key=FILDTEOIK2HBORODV&format=json&results=10&name=train&bucket=id:whosampled
+//http://developer.echonest.com/api/v4/artist/search?api_key=FILDTEOIK2HBORODV&format=json&results=10&name=train&bucket=id:whosampled
 $(document).ready(function() {
     $('input').keydown(function(e) {
         if (e.which == 13) {
@@ -14,17 +14,23 @@ function whosampled(tag){
       type: 'GET',
       data: {
         api_key: 'GASA0WIIJLEC8ZPM4',
-        results: 10,
+        results: 1,
         name: tag,
         bucket: 'id:whosampled'
       },
       success: function(data){
         console.log('success',data);
-        $('.app .info').append(data.response.artists[0].name);
+        $('ul.results').empty();
+          
+        //Split response from whosampled:artist:#### into array to pull the ####
+        var foreignId = data.response.artists[0].foreign_ids[0].foreign_id.split(':');
+          
+        //Then we can build the url and append it to the screen
+        $('ul.results').append("<li><iframe src='http://www.whosampled.com/artist/view/"+ foreignId[2] +"/'></iframe></li>"); 
       },
       error: function(data){
         console.log('error',data);
-         $('.app .info').append(data.response.artists[0].name);
+         $('#app').append(data[10].user_id);
       }
     });
 }
