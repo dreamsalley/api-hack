61ebbef7817df2ec724d8cd653aec6d136b248b8
csdgrevolution <CreativeStar@tisean-bells-macbook.local>
2014-10-25 14:47:22 -0400

    changes

--- js/app.js
+++ js/app.js
@@ -1,4 +1,4 @@
-//http://developer.echonest.com/api/v4/artist/search?api_key=FILDTEOIK2HBORODV&format=json&results=10&name=train&bucket=id:whosampled
+ //http://developer.echonest.com/api/v4/artist/search?api_key=FILDTEOIK2HBORODV&format=json&results=10&name=train&bucket=id:whosampled
 $(document).ready(function() {
     $('input').keydown(function(e) {
         if (e.which == 13) {
@@ -9,24 +9,23 @@ $(document).ready(function() {
 
 function whosampled(tag){
     $.ajax({
-      url: 'http://developer.echonest.com/api/v4/artist/search',
+      url: 'http://developer.echonest.com/api/v4/song/search',
       dataType: 'json',
       type: 'GET',
       data: {
         api_key: 'GASA0WIIJLEC8ZPM4',
-        results: 1,
-        name: tag,
-        bucket: 'id:whosampled'
+        artist: tag,
       },
       success: function(data){
         console.log('success',data);
         $('ul.results').empty();
-          
-        //Split response from whosampled:artist:#### into array to pull the ####
-        var foreignId = data.response.artists[0].foreign_ids[0].foreign_id.split(':');
-          
-        //Then we can build the url and append it to the screen
-        $('ul.results').append("<li><iframe src='http://www.whosampled.com/artist/view/"+ foreignId[2] +"/'></iframe></li>"); 
+        
+        for(var i = 0; i < data.response.songs.length; i++){
+            var artist = data.response.songs[i].artist_name,
+                song_name = data.response.songs[i].title;
+            
+            $('ul.results').append("<li>Artist: "+ artist +", Song Name: "+ song_name +"</li>"); 
+        }
       },
       error: function(data){
         console.log('error',data);
